51dc0de94cd0e9e9dafad09b0211bdf3e288e33a
md-sazzadul <md.sazzadul.islam15@gmail.com>
2022-10-18 11:01:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NotFound from './components/pages/NotFound';
+import AddUser from './components/users/AddUser';
+import EditUser from './components/users/EditUser';
+import User from './components/users/User';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}></Route>
+          <Route exact path="/about" element={<About></About>}></Route>
+          <Route exact path="/contact" element={<Contact></Contact>}></Route>
+          <Route exact path='/users/add' element={<AddUser></AddUser>}></Route>
+          <Route exact path='/users/edit/:id' element={<EditUser></EditUser>}></Route>
+          <Route exact path='/users/:id' element={<User></User>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='container'>
+            <div className='py-4'>
+                <h1>About Page</h1>
+                <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dicta officiis sunt cum maiores illum, ratione accusantium reiciendis, dignissimos deserunt est totam quia? Tempore obcaecati aspernatur, ipsam eius at illum?</p>
+                <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dicta officiis sunt cum maiores illum, ratione accusantium reiciendis, dignissimos deserunt est totam quia? Tempore obcaecati aspernatur, ipsam eius at illum?</p>
+                <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus dicta officiis sunt cum maiores illum, ratione accusantium reiciendis, dignissimos deserunt est totam quia? Tempore obcaecati aspernatur, ipsam eius at illum?</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
